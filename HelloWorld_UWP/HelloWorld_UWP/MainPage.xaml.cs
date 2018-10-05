74a06656ecc6d3be7b66952b9595002ee853caa4
Luc Leysen <luc56.leysen@gmail.com>
2018-10-05 13:24:33 +0200

    hello world

---
+++ HelloWorld_UWP/HelloWorld_UWP/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HelloWorld_UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickMeButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResultTextBlock.Text = "Hello World";
+        }
+    }
+}
